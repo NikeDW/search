1ac004ef14c5f4b8fc31e26485d139f18011f458
NikeSM <abram-999@yandex.ru>
2015-12-24 13:36:14 +0300

    viomedia(test1)

--- viomedia/test1.js
+++ viomedia/test1.js
@@ -1,21 +1,20 @@
 $(document).ready(function() {
     var $window = $(window);
-    var block1 = $('#block1');
-    var line1_position = $('#line1').offset().top;
-    var line2_position = $('#line2').offset().top;
+    var $block1 = $('.standardBlock');
+    var $line = $('.line');
+    var line1_position = $($line[0]).offset().top;
+    var line2_position = $($line[1]).offset().top;
 
     $window.scroll(function() {
-        var scroll = $(window).scrollTop();
+        var scroll = $window.scrollTop();
         if (scroll > line1_position) {
-            block1.css({position: 'absolute'});
-            if (scroll < line2_position - block1.height()) {
-                block1.css({top: scroll});
+            if (scroll < line2_position - $block1.height()) {
+                $block1.addClass('fixedBlock').removeClass('stoppedBlock')
             } else {
-                block1.css({top: line2_position - block1.height()})
+                $block1.addClass('stoppedBlock').removeClass('fixedBlock')
             }
         } else {
-            block1.css({position: 'relative',
-                top: 0});
+            $block1.removeClass('stoppedBlock').removeClass('fixedBlock')
         }
 
     });
